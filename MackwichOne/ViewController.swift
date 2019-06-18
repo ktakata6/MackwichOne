13c322416c9c4484936eaaa83726be6d326bcb04
Kimmy <ktakata6@hawaii.edu>
2019-06-18 13:49:27 -1000

    Initial Commit

---
+++ MackwichOne/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MackwichOne
+//
+//  Created by Kimberly Takata on 6/18/19.
+//  Copyright © 2019 Kimberly Takata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
